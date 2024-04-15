65c37dce59e1fb877d966d13b0005d9e75c94850
Patryk Żmigrodzki <100092805+zmigrOO@users.noreply.github.com>
2024-04-15 10:29:18 +0200

    Update vite.config.ts
    Co-authored-by: Kamil Piech <kamil.piech@blumilk.pl>

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { dirname } from 'path'
 
-// Obtain the directory path of the current file
 const dirName = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
